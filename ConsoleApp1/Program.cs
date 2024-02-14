bfc27fdbb00a804f926575e7cbb2bb6fdb7b05ac
fm Bushry <fmbushry14@gmail.com>
2024-02-14 16:57:38 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string a = "Bushry";
+string b = "faleelullah mohamed";
+string c = "this is ";
+Console.WriteLine(c + b + a);
